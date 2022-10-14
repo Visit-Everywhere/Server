5ef412e9650aeda8be33a57d8903b517a797cc1e
Cisamu <76702667+cisamu123@users.noreply.github.com>
2022-10-14 13:14:42 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,17 +2,21 @@ import express from 'express';
 import Router from './routes/index.js' // all routes
 import DB from './utils/connectMongoose.js' // DB connecting
 
-const app = express();
-const { models } = DB // extracting al models
-const PORT = process.env.PORT || 3000
+try {
+    const app = express();
+    const { models } = DB // extracting al models
+    const PORT = process.env.PORT || 3000
 
-// middlewares
-app.use((req, res, next) => { // now in every request would be our models, we don't need to import everywhere
-    req.models = models
-    next()
-})
+    // middlewares
+    app.use((req, res, next) => { // now in every request would be our models, we don't need to import everywhere
+        req.models = models
+        next()
+    })
 
-// app.use(express.static('public')) // if project is 1 tier
-app.use(Router)
+    // app.use(express.static('public')) // if project is 1 tier
+    app.use(Router)
 
-app.listen(PORT, console.log(`I'm ready on http://localhost:${PORT}`))
+    app.listen(PORT, console.log(`I'm ready on http://localhost:${PORT}`))
+} catch (mainError) {
+    console.log("Main code error: " + mainError)
+}
