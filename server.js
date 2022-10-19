4899bcb485f7fd9ff550458bbd24227b80928363
Xayrulloh <xayrullohabduvohidov713@gmail.com>
2022-10-19 15:22:10 +0500

    testing

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
   req.models = models;
   next();
 });
-
+ 
 // app.use(express.static('public')) // if project is 1 tier
 app.use(cookieParser())
 app.use(express.json());
