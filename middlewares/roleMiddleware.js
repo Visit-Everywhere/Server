b2a920793c6fc78e64fbc80602a2a60537898258
joffy13 <nikikrokha@gmail.com>
2022-11-28 09:57:24 +0200

    fix roleMiddleware

--- middlewares/roleMiddleware.js
+++ middlewares/roleMiddleware.js
@@ -1,24 +1,27 @@
+import tokenService from "#authService/TokenService";
+
 export default (roles) => {
   return (req, res, next) => {
     if (req.method === "OPTIONS") {
         next();
       }
       try {
-        const token = req.headers.authorization.split('')[1]
+        const token = req.headers.authorization.split(' ')[1]
         if(!token){
             return res.status(403).json("Пользователь не авторизован")
         }
-        const {userRoles} = tokenService.validateAccessToken(token);
-        const hasRole = false
-        userRoles.forEach(role => {
-            if(roles.includes(role)){
-                hasRole =true
+        let thisRoles = tokenService.validateAccessToken(token).userRoles
+        let hasRole = false
+        thisRoles.forEach(role => {
+            let rolesToUpper = role.toUpperCase() 
+            if(roles.includes(rolesToUpper)){
+                hasRole = true
             }
         });
         if(hasRole == false){
             return res.status(403).json("У тебя нет прав чёрт")
         }
-        next
+        next()
         } catch (err) {
             return res.status(403).json(`hui error${err}`)
       }
